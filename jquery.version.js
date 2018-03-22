dfaf425df412c0c3ebb3e55905b4fea62efc78b1
何明明 <mingming.he@bl.com>
2018-03-22 15:37:24 +0800

    jQuery 1.0.0 & 1.0.1

---
+++ jquery.version.js
@@ -0,0 +1,23 @@
+/*
+ * jQuery version learning @ Ming
+ *
+ *
+
+@版本                                                                                      				@标记注释
+
+
+jquery 1.0.0                				jquery1.0.1
+
+if ($)                      				if (typeof $ != "undefined")                   							// $符判断
+                            				if ( r[i].nodeType != 8 )                      							// text 
+                            				"nth-child": "jQuery.sibling(a,m[3]).cur",     							// expr 
+                            				if ( foundToken ) continue;                    							// find 
+						    				value: "value",                                							// attr 
+						    				disabled: "disabled"							
+children: "a.childNodes"					children: "jQuery.sibling(a.firstChild)"       							// jQuery.macros axis
+if ( !a || jQuery.filter( [this], a ).r )   if ( !a || jQuery.filter( a, [this] ).r )      							// remove
+if ( this.readyState == "complete" )        if ( this.readyState != "complete" ) return;   							// new
+			                                this.parentNode.removeChild( this );   
+if (z.now == 1) z.now = 0.9999;             if (jQuery.browser.mozilla && z.now == 1) z.now = 0.9999;				// fix
+if ( jQuery.browser.msie )                  if ( jQuery.browser.msie && typeof XMLHttpRequest == "undefined" )		// If IE is used, create a wrapper for the XMLHttpRequest object
+                                            if ( type == "json" ) eval( "data = " + data );							// Get the JavaScript object, if JSON is used.
